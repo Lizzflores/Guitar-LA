13e9b7116ac641115d539c48a5e2aaaeb178a711
ElizabethFlores <lizflores.dev@gmail.com>
2024-08-04 22:14:10 -0600

    feat: Add addToCart function

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ import { db } from "./data/db.js";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExists >= 0) {
+      //existe en el carrito
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
   return (
     <>
@@ -15,7 +30,12 @@ function App() {
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,5 +1,6 @@
-export default function Guitar({ guitar }) {
+export default function Guitar({ guitar, addToCart }) {
   const { id, name, image, description, price } = guitar;
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
@@ -13,7 +14,11 @@ export default function Guitar({ guitar }) {
         <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
         <p>{description}</p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button
+          type="button"
+          className="btn btn-dark w-100"
+          onClick={() => addToCart(guitar)}
+        >
           Agregar al Carrito
         </button>
       </div>
