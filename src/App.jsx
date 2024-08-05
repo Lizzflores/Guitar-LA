8071aa166fd2ce0adf6e70da83f581f58383a62a
ElizabethFlores <lizflores.dev@gmail.com>
2024-08-04 22:30:20 -0600

    feat: Display guitars on cart

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header cart={cart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-export default function Header() {
+export default function Header({ cart }) {
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -33,31 +33,33 @@ export default function Header() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>
-                        <img
-                          className="img-fluid"
-                          src="./public/img/guitarra_02.jpg"
-                          alt="imagen guitarra"
-                        />
-                      </td>
-                      <td>SRV</td>
-                      <td className="fw-bold">$299</td>
-                      <td className="flex align-items-start gap-4">
-                        <button type="button" className="btn btn-dark">
-                          -
-                        </button>
-                        1
-                        <button type="button" className="btn btn-dark">
-                          +
-                        </button>
-                      </td>
-                      <td>
-                        <button className="btn btn-danger" type="button">
-                          X
-                        </button>
-                      </td>
-                    </tr>
+                    {cart.map((guitar) => (
+                      <tr key={guitar.id}>
+                        <td>
+                          <img
+                            className="img-fluid"
+                            src={`/img/${guitar.image}.jpg`}
+                            alt="imagen guitarra"
+                          />
+                        </td>
+                        <td>{guitar.name}</td>
+                        <td className="fw-bold">${guitar.price}</td>
+                        <td className="flex align-items-start gap-4">
+                          <button type="button" className="btn btn-dark">
+                            -
+                          </button>
+                          {guitar.quantity}
+                          <button type="button" className="btn btn-dark">
+                            +
+                          </button>
+                        </td>
+                        <td>
+                          <button className="btn btn-danger" type="button">
+                            X
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
 
